6c10d314a232ef9be42916ef1be2f629b0d36a53
Anastasia Dimakopoulou <anadimakopoulou@gmail.com>
2020-09-26 11:15:10 +0300

    week 1 & 2 of 3rd course

---
+++ Concurrency-in-go/simplegoroutines.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(x *int, name string) {
+	*x = *x + 1
+	fmt.Printf("Name %s: x=%d\n", name, *x)
+}
+
+func main() {
+	x := 0
+	for i := 0; i < 5; i++ {
+		go f(&x, "first")
+		go f(&x, "second")
+		time.Sleep(5 * time.Millisecond)
+		x += 1
+	}
+	fmt.Printf("Main: x=%d\n", x)
+}
